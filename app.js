ac835a3de2bd0d4532a71d03fa2968e664a06d74
LykeWise <54202433+LykeWise@users.noreply.github.com>
2019-09-15 22:47:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,4 +9,47 @@ function openNav() {
     document.getElementById("sidemenue").style.opacity= "1.0";
   }
 
- 
+  var myIndex = 0;
+
+  
+  function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";
+    x[myIndex-1].style.width = "50%"; 
+    setTimeout(carousel, 3500); // Change image every 2 seconds
+  }
+
+  var slideIndex = 1;
+
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";
+  x[myIndex-1].style.width = "50%"; 
+}
+
+  function openCity(cityName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("solidt");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    
+    document.getElementById(cityName).style.display = "block";
+  }
